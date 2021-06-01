b4101ca83b5cd0bae0cb64258e36d7784f2d39e0
Italo Sena <saldograduado@gmail.com>
2021-06-01 15:42:59 -0300

    Mask Plugin Edited

--- src/plugins/Mask/index.ts
+++ src/plugins/Mask/index.ts
@@ -3,19 +3,19 @@ import MaskResolver from './mask-resolver';
 
 
 export default class Mask {
-  static toMask(type, value, settings) {
+  static toMask(type: any, value: any, settings?: any) {
     return MaskResolver.resolve(type).getValue(value, settings);
   }
 
-  static toRawValue(type, maskedValue, settings) {
+  static toRawValue(type: any, maskedValue: any, settings?: any) {
     return MaskResolver.resolve(type).getRawValue(maskedValue, settings);
   }
 
-  static isValid(type, value, settings) {
+  static isValid(type: any, value: any, settings?: any) {
     return MaskResolver.resolve(type).validate(value, settings);
   }
 
-  static getMask(type, value, settings) {
+  static getMask(type: any, value: any, settings?: any) {
     return MaskResolver.resolve(type).getMask(value, settings);
   }
 }
